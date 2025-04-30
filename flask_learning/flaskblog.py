6ff91abb02a04ec08153daf87d916a7645066c86
Mohammed Ibn Ameen <94846104+coderwoder@users.noreply.github.com>
2025-04-30 20:46:12 +0000

    SQLalchmey, Created User&Post model

--- flask_learning/flaskblog.py
+++ flask_learning/flaskblog.py
@@ -1,7 +1,35 @@
+from datetime import datetime,timezone
 from flask import Flask,render_template,url_for,flash,redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4d92f5816bda1d4bafa57beb34036069'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db= SQLAlchemy(app)
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True,)
+    username=db.Column(db.String(20),unique=True,nullable=False)
+    email=db.Column(db.String(150),unique=True,nullable=False)
+    image_file=db.Column(db.String(20),nullable=False,default='default.jpg')
+    password=db.Column(db.String(60),nullable=False)
+    post=db.relationship('Post',backref='author',lazy=True)
+
+    def __repr__(self):
+        return f"User({self.username},{self.email},{self.image_file})"
+
+class Post(db.Model):
+    id= db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(150),nullable=False)
+    date_posted=db.Column(db.DateTime,nullable=False,default=datetime.now(timezone.utc)) # Not the () since we don't want the (executed) time right now
+    content=db.Column(db.Text,nullable=False)
+    # The 'User' model will have atomatically set the table name as 'user' same for 'Post'->'post, tb_name can be changed. 
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)  
+
+    def __repr__(self):
+        return f"Post({self.title},{self.date_posted})"
+    
 
 posts = [
     {
